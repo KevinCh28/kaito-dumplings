46222213be17a285ad749afe015b72d3e001b683
Kevin Chen <kevinch28@gmail.com>
2022-12-27 18:21:12 -0500

    update login page component

---
+++ client/src/components/session/login_page.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import type { NextPage } from 'next';
+
+const LoginPage: NextPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
+
+  const handleSubmit = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+
+    const res = await fetch('/api/auth/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, password }),
+    });
+
+    if (res.ok) {
+      router.push('/');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="email">Email</label>
+      <input
+        type="email"
+        name="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        type="password"
+        name="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Log In</button>
+    </form>
+  );
+};
+
+export default LoginPage;
