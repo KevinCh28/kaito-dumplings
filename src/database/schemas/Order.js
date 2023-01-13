ed0e5b0bce246e710a53ae3bb4810faed8b0ee3d
Kevin Chen <kevinch28@gmail.com>
2023-01-13 16:41:26 -0500

    add order schema

---
+++ src/database/schemas/Order.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const OrderSchema = new Schema({
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  orderNumber: {
+    type: String,
+    require: true
+  },
+  total: {
+    type: Number,
+    require: true
+  },
+  items: [{
+    product: {
+      type: Schema.Types.ObjectId,
+      ref: 'Product'
+    },
+    quantity: {
+      type: Number,
+      default: 1
+    }
+  }],
+  date: {
+    type: Date,
+    default: Date.now
+  }
+}, {
+  timestamps: true
+});
+
+export default mongoose.model('Order', OrderSchema);
