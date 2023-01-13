0debf0f70d311e30c799c74d9377fc319f19dd7f
Kevin Chen <kevinch28@gmail.com>
2023-01-13 16:44:38 -0500

    add category schema

---
+++ src/database/schemas/Category.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const CategorySchema = new Schema({
+  name: {
+    type: String,
+    enum: ['Dumplings', 'Gyoza'],
+    default: 'Dumplings',
+    required: true
+  },
+}, {
+  timestamps: true
+});
+
+export default mongoose.model('Category', CategorySchema);
