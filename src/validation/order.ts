ecd3988fa469d5b82481b81d56eddb06fbf1fbdd
Kevin Chen <kevinch28@gmail.com>
2023-02-01 13:55:36 -0500

    update order validation

--- src/validation/order.ts
+++ src/validation/order.ts
@@ -5,6 +5,11 @@ const validateOrderInput = [
   check('products')
     .exists({ checkFalsy: true })
     .withMessage('Order must contain at least one product'),
+  check('orderStatus')
+    .exists({ checkFalsy: true })
+    .withMessage('Order status is required')
+    .isIn(['pending', 'transit', 'completed', 'refunded', 'canceled'])
+    .withMessage('Order status must be either pending, transit, completed, refunded, or canceled'),
   handleValidationErrors,
 ];
 
