79281eb8049641ea3c5208cc7589d722ce0d4e8c
Kevin Chen <kevinch28@gmail.com>
2023-02-07 10:42:42 -0500

    add  schemas for User Cart and Product

---
+++ src/database/schemas/Cart.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CartSchema = new Schema({
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  // Array of Product Ids and quantity
+  products: [
+    {
+      productId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Product'
+      },
+      quantity: {
+        type: Number,
+        required: true
+      }
+    }
+  ],
+  totalPrice: {
+    type: Number,
+    required: true
+  }
+});
+
+module.exports = Cart = mongoose.model('Cart', CartSchema);
--- src/database/schemas/User.js
+++ src/database/schemas/User.js
@@ -2,7 +2,25 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-
+  firstname: {
+    type: String,
+    required: true
+  },
+  lastname: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  cart: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Cart'
+  }]
+}, {
+  timestamps: true
 });
 
 module.exports = User = mongoose.model('User', UserSchema);
