5f1f6dd5e07f2117af09967502e3a5de13724752
Kevin Chen <kevinch28@gmail.com>
2023-02-09 22:22:03 -0500

    add handle errors

--- client/src/pages/register/index.tsx
+++ client/src/pages/register/index.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import Link from 'next/link';
-import 
+import { register } from "../../utils/sessionApiUtils";
 
 const Register = () => {
   const [user, setUser] = useState({
@@ -28,8 +28,88 @@ const Register = () => {
   }
 
   const handleSubmit = (e) => {
+    e.preventDefault();
+    register(user)
+      .then((res) => {
+        window.location.href = "/";
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
+  const renderErrors = () => {
+    if (!Object.values(errors).every((val) => val === "")) {
+      <div>
+        <ul>
+          {Object.values(errors).map((val, idx) => (
+            <li key={idx}>{val}</li>
+          ))}
+        </ul>
+      </div>
+    } else {
+      return null;
+    }
+  };
 
+  return (
+    <div>
+      <div>
+        <h1>Register</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="firstName">First Name</label>
+            <input
+              type="text"
+              name="firstName"
+              value={user.firstName}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="lastName">Last Name</label>
+            <input
+              type="text"
+              name="lastName"
+              value={user.lastName}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              name="email"
+              value={user.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              value={user.password}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="password2">Confirm Password</label>
+            <input
+              type="password"
+              name="password2"
+              value={user.password2}
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit">Register</button>
+        </form>
+      </div>
+      <div>
+        <Link href="/login">Login</Link>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
