c9bb03a2cd83183a71c15b55e68f9b02dadc01ed
unknown <kevinch28@gmail.com>
2023-02-28 16:05:28 -0500

    add front end get product api

--- client/src/utils/productApiUtils.tsx
+++ client/src/utils/productApiUtils.tsx
@@ -1,11 +1,17 @@
+import axios from "axios";
+
 export const getProducts = async () => {
   const data = await fetch("/api/products");
   const res = await data.json();
   return res;
 };
 
-export const getProduct = async (productId: any) => {
-  const data = await fetch(`/api/products/${productId}`);
-  const res = await data.json();
-  return res;
+// export const getProduct = async (productId: any) => {
+//   const data = await fetch(`/api/products/${productId}`);
+//   const res = await data.json();
+//   return res;
+// };
+
+export const getProduct = async (productName: string) => {
+  return await axios.get(`/api/products/${productName}`);
 };
