b9ec4d125635062f234ec83edcdafc69405c1a5c
unknown <kevinch28@gmail.com>
2023-02-28 19:57:18 -0500

    update products dynamic route to redirect to products index page if url route does not match

--- client/src/pages/products/[productName].tsx
+++ client/src/pages/products/[productName].tsx
@@ -1,12 +1,23 @@
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 const ProductDetail = () => {
     const router = useRouter();
-    const productName = router.query.productName;
-    
-    return (
-        <div>{productName}</div>
-    );
+    const { productName } = router.query;
+    const products = {
+            'veggie': 'Veggie (Vegan) (50pc)',
+            'chicken-&-cabbage': 'Chicken & Cabbage (50pc)',
+            'beef-&-cheese': 'Beef & Cheese (50pc)',
+            'pork-&-chieves': 'Pork & Chieves (50pc)'
+        };
+
+    useEffect(() => {
+        if (!products[productName]) {
+            router.push('/products');
+        } else {
+            return <div>{productName}</div>
+        };
+    }, [productName, router.isReady]);
 };
 
 export default ProductDetail;
