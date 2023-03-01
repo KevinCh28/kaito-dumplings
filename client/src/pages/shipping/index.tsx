99872130e6f3ae335d199e96b62891981e9487db
unknown <kevinch28@gmail.com>
2023-02-28 22:02:58 -0500

    update shipping info

--- client/src/pages/shipping/index.tsx
+++ client/src/pages/shipping/index.tsx
@@ -1,7 +1,26 @@
 export default function Shipping() {
   return (
     <div>
-      <h1>Shipping!</h1>
+      <div>
+        <h1>HOW DO YOUR DUMPLINGS STAY FROZEN?</h1>
+      </div>
+      <div>
+        <h1>WHEN WILL MY DUMPLINGS ARRIVE?</h1>
+        <p>We ship orders Mondays - Thursdays and we aim to ship out dumplings within 1-3 business days after your order is placed. This shipping setup ensures your dumplings remain safe and frozen while traveling with blocks of dry-ice!</p>
+        <p>Once shipped, your dumplings will arrive fresh to your home within 2 days. If not, please reach out to us and our team will make things right.</p>
+      </div>
+      <div>
+        <h1>WHAT IS BEHIND THE SHIPPING COST?</h1>
+        <p>Frozen shipping is expensive due to dry ice and insulated liner costs but we do a lot of work on our back end to bring the cost down and pass on savings to you!</p>
+        <h4>We work hard every day to bring down costs even more.</h4>
+        <p>We are proud to be able to offer free shipping on orders over $99 (whereas other frozen foods may charge $40+ per order or add it into product costs.) Not here.</p>
+        <p>For those interested, here is how we make it work logistically:</p>
+        <p><h4>1.</h4>All our dumplings are made fresh in each and every morning in our Seattle kitchen.</p>
+        <p><h4>2.</h4>We strategically ship dumplings to dumpling depots all around the country</p>
+        <p><h4>3.</h4>This allows us to offer cheap ground shipping to the majority of our customers instead of resorting to more expensive air shipping.</p>
+        <p><h4>4.</h4>Our product is typically made only a week before your order is made to maximize freshness!</p>
+        <p>It typically costs us a fixed $35 for ground shipments but we are happy to pay the majority of costs on our end. In order to sustainably do this, we rely on our happy customers to spread the word instead of using a large marketing budget - please help us spread the word to your friends and family!</p>
+      </div>
     </div>
   );
 };
