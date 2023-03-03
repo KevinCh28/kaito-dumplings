5b1f66ab78c83785926ac6fb73490611964cdcaa
Kevin Chen <kevinch28@gmail.com>
2023-03-03 12:48:24 -0500

    add get cart frontend route

--- client/src/components/cart/index.tsx
+++ client/src/components/cart/index.tsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react';
 import { getProductsByIds } from '../../utils/productApiUtils';
 
+
 const Cart = (cart: any) => {
   const [cartItems, setCartItems] = useState([]);
   
+
   useEffect(() => {(
     async () => {
       try {
--- client/src/utils/cartApiUtils.tsx
+++ client/src/utils/cartApiUtils.tsx
@@ -3,3 +3,7 @@ import axios from "axios";
 export const createCart = async (userId: string) => {
   return await axios.post("/api/carts", { userId });
 };
+
+export const getCart = async (userId: string) => {
+  return await axios.get(`/api/carts/${userId}`);
+};
