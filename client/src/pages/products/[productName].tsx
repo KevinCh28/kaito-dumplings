b072c9f7003c9e5f50d78b887451c548d7df07d0
Kevin Chen <kevinch28@gmail.com>
2023-03-04 10:29:51 -0500

    update indentation

--- client/src/pages/products/[productName].tsx
+++ client/src/pages/products/[productName].tsx
@@ -1,23 +1,23 @@
 import { useRouter } from 'next/router';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 
 const ProductDetail = () => {
-    const router = useRouter();
-    const { productName } = router.query;
-    const products = {
-            'veggie': true,
-            'chicken-&-cabbage': true,
-            'beef-&-cheese': true,
-            'pork-&-chieves': true
-        };
+  const router = useRouter();
+  const { productName } = router.query;
+  const products = {
+    'veggie': true,
+    'chicken-&-cabbage': true,
+    'beef-&-cheese': true,
+    'pork-&-chieves': true
+  };
 
-    useEffect(() => {
-        if (!products.productName) {
-            router.push('/products');
-        } else {
-            return <div>{productName}</div>
-        };
-    }, [productName, router.isReady]);
+  useEffect(() => {
+    if (!products.productName) {
+      router.push('/products');
+    } else {
+      return <div>{productName}</div>
+    };
+  }, [productName, router.isReady]);
 };
 
 export default ProductDetail;
--- client/src/pages/products/beef-&-cheese.tsx
+++ client/src/pages/products/beef-&-cheese.tsx
@@ -2,39 +2,39 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 const BeefCheese = () => {
-    const router = useRouter();
-    const productName = router.query.productName;
-    const flavors = [
-        'veggie',
-        'chicken-&-cabbage',
-        'beef-&-cheese',
-        'pork-&-chieves'
-    ];
+  const router = useRouter();
+  const productName = router.query.productName;
+  const flavors = [
+    'veggie',
+    'chicken-&-cabbage',
+    'beef-&-cheese',
+    'pork-&-chieves'
+  ];
 
-    const renderFlavors = () => {
-        return flavors.map((flavor) => {
-            if (flavor === productName) {
-                return <div key={flavor}>{flavor.split('-').join(' ').toUpperCase()}</div>
-            } else {
-                return (
-                    <div key={flavor}>
-                        <Link href={`/products/${flavor}`}>
-                            {flavor.split('-').join(' ').toUpperCase()}
-                        </Link>
-                    </div>
-                );
-            }
-        });
-    };
+  const renderFlavors = () => {
+    return flavors.map((flavor) => {
+      if (flavor === productName) {
+        return <div key={flavor}>{flavor.split('-').join(' ').toUpperCase()}</div>
+      } else {
+        return (
+          <div key={flavor}>
+            <Link href={`/products/${flavor}`}>
+              {flavor.split('-').join(' ').toUpperCase()}
+            </Link>
+          </div>
+        );
+      }
+    });
+  };
 
-    return (
-        <div>
-            <h1>BEEF & CHEESE (50PC)</h1>
-            <div>$44.95</div>
-            <div>{renderFlavors()}</div>
-            <div>Description</div>
-        </div>
-    );
+  return (
+    <div>
+      <h1>BEEF & CHEESE (50PC)</h1>
+      <div>$44.95</div>
+      <div>{renderFlavors()}</div>
+      <div>Description</div>
+    </div>
+  );
 };
 
 export default BeefCheese;
