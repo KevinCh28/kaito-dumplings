9c6ce9d7efc1f3ce837df51936dec311625e6c16
Kevin Chen <kevinch28@gmail.com>
2023-03-03 22:28:13 -0500

    add global button styling and update cart buttons styling

--- client/src/components/modal/modal.tsx
+++ client/src/components/modal/modal.tsx
@@ -1,4 +1,4 @@
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faTimes, faLock } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useState, useEffect, useRef } from 'react';
 
@@ -12,11 +12,33 @@ const Modal = ({ onClose = () => {}, children }) => {
             <h2 className='modal_header_text'>YOUR CART</h2>
           </div>
           <div className='modal_header_close_button' onClick={onClose}>
-            <i className='fa fa-times'><FontAwesomeIcon icon={faTimes}></FontAwesomeIcon></i>
+            <i><FontAwesomeIcon icon={faTimes}></FontAwesomeIcon></i>
           </div>
         </div>
         {children}
+        <div className='cart_summary_container'>
+          <div className='cart_sub_total'>
+            <div className='sub_total_text'>
+              <span>Subtotal ( items)</span>
+            </div>
+            <div className='subtotal_amount'>$</div>
+          </div>
+
+          <div className='cart_action_buttons_container'>
+            <button className='cart_checkout_button'>
+              <span><i className='cart_checkout_button_image'><FontAwesomeIcon icon={faLock}></FontAwesomeIcon></i> Checkout </span>
+            </button>
+            <button className='cart_continue_shopping_button' onClick={onClose}>
+              <span>Continue Shopping</span>
+            </button>
+          </div>
+        </div>
+
+        <div>
+
+        </div>
       </div>
+      {/* <div className='modal_background'></div> */}
     </div>
   )
 };
