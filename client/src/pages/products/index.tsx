1d9bd00aaf753fc261b53a621d373b0c85875073
Kevin Chen <kevinch28@gmail.com>
2023-03-04 14:38:08 -0500

    add frontend cart routes and update products page

--- client/src/pages/products/index.tsx
+++ client/src/pages/products/index.tsx
@@ -1,22 +1,59 @@
 import Link from 'next/link';
 import { useState, useEffect } from "react";
-import { getProducts } from "../../utils/productApiUtils";
+import { getCurrentUser } from '../../utils/sessionApiUtils';
+import { increaseItemQuantity } from "../../utils/cartApiUtils";
+import Modal from '../../components/modal/modal';
+import Cart from '../../components/cart/cart';
 
 const Products = () => {
-  const [products, setProducts] = useState([]);
-  const [dumplings, setDumplings] = useState([]);
-  const [gyozas, setGyozas] = useState([]);
+  const dumplings = {
+    'beef-&-cheese': '63efa9419010d97ce1747161',
+    'chicken-&-cabbage': '63efa96f9010d97ce1747163',
+    'pork-&-chieves': '63efa8d89010d97ce1747159',
+    'veggie': '63efa9119010d97ce174715c'
+  }
+  const gyoza = {
+    'beef-&-cheese': '63efa8319010d97ce1747153',
+    'chicken-&-cabbage': '63efa8b49010d97ce1747155',
+    'pork-&-chieves': '63efa8d19010d97ce1747157',
+    'veggie': '63efa9259010d97ce174715f'
+  }
+  const [showModal, setShowModal] = useState(false);
+  const [userId, setUserId] = useState('6402bbea4526f7ce1ddf7470');
+  const [dumplingsId, setDumplingsId] = useState('63efa9419010d97ce1747161');
+  const [gyozaId, setGyozaId] = useState('63efa8319010d97ce1747153');
 
-  useEffect(() => {
-    getProducts().then((res) => {
-      setProducts(res);
-    });
-  }, []);
+  // useEffect(() => {
+  //   getCurrentUser()
+  //     .then((res) => {
+  //       setUserId(res.data._id);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }, []);
 
-  useEffect(() => {
-    setDumplings(products.filter((product) => product.category === "dumplings"));
-    setGyozas(products.filter((product) => product.category === "gyoza"));
-  }, [products]);
+  const handleAddToCart = (e: { preventDefault: () => void; target: { value: any; }; }) => {
+    e.preventDefault();
+    const productId = e.target.value;
+    increaseItemQuantity(userId, productId)
+      .then((res) => {
+        setShowModal(true);
+      })
+      .catch((err) => {
+        console.log(err);
+      }
+    );
+  };
+
+  const handleCartModal = () => {
+    if (!showModal) return null
+    return (
+      <Modal onClose={() => setShowModal(false)} >
+        <Cart userId={props.user._id} />
+      </Modal>
+    )
+  };
 
   return (
     <main className='products_page_main'>
@@ -49,7 +86,7 @@ const Products = () => {
                                   <span className='products_page_item_price'>$44.95</span>
                                 </div>
                                 <h3 className='products_page_item_name'>
-                                  <a href="/products/beef-&-cheese">DUMPLINGS</a>
+                                  <a href="/products/beef-&-cheese">DUMPLINGS (50 PC)</a>
                                 </h3>
                                 <div>
                                   <ul className='products_page_item_descriptions'>
@@ -90,7 +127,7 @@ const Products = () => {
 
                               </div>
                               <div className='products_page_add_button_container'>
-                                <button className='products_page_add_button'>
+                                <button className='products_page_add_button' onClick={handleAddToCart}>
                                   <span>ADD TO CART</span>
                                   <span>
                                     <div>
@@ -133,6 +170,7 @@ const Products = () => {
           <div><a href=""></a></div>
         </div>
       </div>
+      {handleCartModal()}
     </main>
   );
 };
--- client/src/utils/cartApiUtils.tsx
+++ client/src/utils/cartApiUtils.tsx
@@ -7,3 +7,16 @@ export const createCart = async (userId: string) => {
 export const getCart = async (userId: string) => {
   return await axios.get(`/api/carts/${userId}`);
 };
+
+export const removeItemFromCart = async (userId: string, productId: string) => {
+  return await axios.delete(`/api/carts/${userId}/${productId}`);
+};
+
+export const increaseItemQuantity = async (userId: string, productId: string) => {
+  return await axios.put(`/api/carts/${userId}/increase`, { productId });
+}
+
+export const lowerItemQuantity = async (userId: string, productId: string) => {
+  return await axios.put(`/api/carts/${userId}/lower`, { productId });
+}
+
