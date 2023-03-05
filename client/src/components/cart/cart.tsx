9ab50e62ec1207725e51e402dd18a974d34aad62
Kevin Chen <kevinch28@gmail.com>
2023-03-05 13:06:01 -0500

    add cart items styling

--- client/src/components/cart/cart.tsx
+++ client/src/components/cart/cart.tsx
@@ -1,29 +1,61 @@
 import { useState, useEffect, SyntheticEvent } from 'react';
 import { getCart } from '../../utils/cartApiUtils';
+import { getCurrentUser } from '@/src/utils/sessionApiUtils';
+import { getProducts } from '@/src/utils/productApiUtils';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMinus, faPlus, faTrash } from '@fortawesome/free-solid-svg-icons';
 
-const Cart = (userId: string) => {
+const Cart = () => {
   const [cart, setCart] = useState([]);
   const [totalAmount, setTotalAmount] = useState(0);
+  const [userId, setUserId] = useState('');
+  const [products, setProducts] = useState([]);
+
+  // Gets user's id
+  useEffect(() => {
+    getCurrentUser().then((res) => {
+      setUserId(res._id);
+    })
+  }, []);
   
+  // Gets user's cart
   useEffect(() => {
     getCart(userId)
       .then((res) => {
-        // console.log(res)
-        if (res.products.length > 0) {
-          setCart(res.products);
-        }
-        // console.log(cart)
+        console.log(res.data.products);
+        setCart(res.data.products);
       })
       .catch((err) => {
-        console.log(err);
+        console.log(err)
       })
+  }, [userId]);
+
+  // Gets all products
+  useEffect(() => {
+    getProducts().then((res) => {
+      console.log(res);
+      setProducts(res);
+    })
   }, []);
+
+  // Calculates $ subtotal of cart
+  useEffect(() => {
+    let total = 0;
+    cart.forEach((item: any) => {
+      products.forEach((product: any) => {
+        if (item.productId === product._id) {
+          total += product.price * item.quantity;
+        }
+      })
+    })
+    setTotalAmount(total);
+  }, [cart, products]);
 
   // Updates free shipping progress bar
   useEffect(() => {
     const progressBar = document.getElementById('free_shipping_status_bar_progress');
     progressBar.style.width = totalAmount > 98.99 ? '100%' : `${totalAmount}%`;
-  }, [])
+  }, [totalAmount])
 
   // Updates free shipping progress text
   const handleCartFreeShippingMessage = () => {
@@ -44,73 +76,66 @@ const Cart = (userId: string) => {
     }
   };
 
+  // Renders cart items
   const handleCartItems = () => {
     if (cart.length > 0) {
       return cart.map((item: any) => {
-        <div className='cart_items_container'>
-          <ul className='cart_items'>
-            {cart.map((item: any) => {
-              return (
-                <li key={item._id}>
-                  <div className='item_image_container'>
-                    <a href="" >
-                      <img src={item.imageUrl} alt={item.name} />
-                    </a>
-                  </div>
-                  <div className='cart_item_info'>
-                    <button className='cart_item_remove_button'>
-                      <i className='fa-trash'></i>
-                      <span className='hiddenSpan'>Remove {item.category} {item.name} from Cart</span>
-                    </button>
-                    <a href="" className='cart_item_category'>{item.category}</a>
-                    <div className='cart_item_name'>{item.name}</div>
-
-                    <div className='item_quantity_container'>
-                      <div className='item_quantity_wrapper'>
-                        <button className='item_quantity_button'>
-                          <i className='fa-minus'></i>
-                          <span className='hiddenSpan'></span>
-                        </button>
-                        <span className='item_quantity_button'></span>
-                        <button className='item_quantity_button'>
-                          <i className='fa-plus'></i>
-                          <span className='hiddenSpan'></span>
-                        </button>
+        return (
+          <div className='cart_items_container'>
+            <ul className='cart_items'>
+              {products.map(product => {
+                if (item.productId === product._id) {
+                  return (
+                    <li key={product._id}>
+                      <div className='item_image_container'>
+                        <a href="" >
+                          <img src={product.imageUrl} alt={product.name} />
+                        </a>
                       </div>
-                    </div>
+                      <div className='cart_item_info'>
+                        <button className='cart_item_remove_button'>
+                          <i><FontAwesomeIcon icon={faTrash}></FontAwesomeIcon></i>
+                          <span className='hiddenSpan'>Remove {product.category} {product.name} from Cart</span>
+                        </button>
+                        <a href="" className='cart_item_category'>{product.category}</a>
+                        <div className='cart_item_name'>{product.name}</div>
 
-                    <div className='item_total_price'>
-                      <div>
-                        <span></span>
+                        <div className='item_quantity_container'>
+                          <div className='item_quantity_wrapper'>
+                            <button className='item_quantity_button'>
+                              <i><FontAwesomeIcon icon={faMinus}></FontAwesomeIcon></i>
+                              <span className='hiddenSpan'></span>
+                            </button>
+                            <span className='item_quantity_button'>{item.quantity}</span>
+                            <button className='item_quantity_button'>
+                              <i><FontAwesomeIcon icon={faPlus}></FontAwesomeIcon></i>
+                              <span className='hiddenSpan'></span>
+                            </button>
+                          </div>
+                        </div>
+
+                        <div className='item_total_price'>
+                          <div>
+                            <span>${(product.price * item.quantity).toFixed(2)}</span>
+                          </div>
+                        </div>
+
                       </div>
-                    </div>
-
-                  </div>
-                </li>
-              )}
-            )}
-          </ul>
-          <div className='cart_products_recommendations_container'></div>
-        </div>
-      })
-    } else {
-      return (
-        <div className='cart_items_container'>
-          <div className="cart_empty">
-            <h4>YOUR CART IS EMPTY!</h4>
-            <p>Add some dumplings and gyozas.</p>
-            <p>
-              <a href="/products" className='cart_empty_shop_now_button'>Shop Now</a>
-            </p>
+                    </li>
+                  )
+                }
+              })
+              };
+            </ul>
+            <div className='cart _products_recommendations_container'>Recommendations</div>
           </div>
-        </div>
-      )
+        )
+      })
     }
-  };
+  }
 
   const handleAddQuantity = (e: SyntheticEvent) => {
     e.preventDefault();
-    console.log(e)
     // setCart();
   };
 
