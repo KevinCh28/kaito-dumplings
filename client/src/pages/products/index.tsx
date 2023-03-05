766c60dc2304a1ecb3f7bb4235f55141b7c7df68
Kevin Chen <kevinch28@gmail.com>
2023-03-05 10:47:33 -0500

    add two products

--- client/src/pages/products/index.tsx
+++ client/src/pages/products/index.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link';
 import { useState, useEffect } from "react";
-import { getCurrentUser } from '../../utils/sessionApiUtils';
 import { increaseItemQuantity } from "../../utils/cartApiUtils";
 import Modal from '../../components/modal/modal';
 import Cart from '../../components/cart/cart';
@@ -123,6 +122,156 @@ const Products = () => {
                                   </div>
                                 </div>
                               </div>
+
+                              </div>
+                              <div className='products_page_add_button_container'>
+                                <button className='products_page_add_button' onClick={handleAddToCart}>
+                                  <span>ADD TO CART</span>
+                                  <span>
+                                    <div>
+                                      <div></div>
+                                    </div>
+                                  </span>
+                                </button>
+                              </div>
+                            </div>
+                          </form>
+                        </div>
+                      </div>
+                    </div>
+                    <div className='products_page_item_container'>
+                      <div className='products_page_item_info_container'>
+                        <div className='products_page_item_image_wrapper'>
+                          <div>
+                            <div className='products_page_item_image'>
+                              <a href="">
+                                <img src="" alt="" />
+                              </a>
+                            </div>
+                          </div>
+                        </div>
+                        <div className='products_page_item_text_wrapper'>
+                          <form action="post">
+                            <input type="hidden" />
+                            <div className='products_page_item_details_wrapper'>
+                              <div className='products_page_item_details_container'>
+                                <div className='products_page_item_price_container'>
+                                  <span className='products_page_item_price'>$44.95</span>
+                                </div>
+                                <h3 className='products_page_item_name'>
+                                  <a href="/products/beef-&-cheese">DUMPLINGS (50 PC)</a>
+                                </h3>
+                                <div>
+                                  <ul className='products_page_item_descriptions'>
+                                    <li className='products_page_item_description'>
+                                      <p>50 Dumplings (Good for 6 Meals!)</p>
+                                    </li>
+                                    <li className='products_page_item_description'>
+                                      <p>Ready in Just 11 Minutes</p>
+                                    </li>
+                                    <li className='products_page_item_description'>
+                                      <p>Steamer Liners Included</p>
+                                    </li>
+                                  </ul>
+                                </div>
+
+                                <div>
+                                  <div>
+                                    <div>
+                                      <div>
+                                        <input type="hidden" />
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+
+                                <div className='products_page_product_rating_container'>
+                                  <div className='products_page_product_ratings'>
+                                    <div className='products_page_product_rating'>
+                                      <svg className='products_page_product_rating_star'><use></use></svg>
+                                      <svg className='products_page_product_rating_star'><use></use></svg>
+                                      <svg className='products_page_product_rating_star'><use></use></svg>
+                                      <svg className='products_page_product_rating_star'><use></use></svg>
+                                      <svg className='products_page_product_rating_star'><use></use></svg>
+                                      <span className='products_page_product_rating_amount'>(2018)</span>
+                                    </div>
+                                  </div>
+                                </div>
+
+                              </div>
+                              <div className='products_page_add_button_container'>
+                                <button className='products_page_add_button' onClick={handleAddToCart}>
+                                  <span>ADD TO CART</span>
+                                  <span>
+                                    <div>
+                                      <div></div>
+                                    </div>
+                                  </span>
+                                </button>
+                              </div>
+                            </div>
+                          </form>
+                        </div>
+                      </div>
+                    </div>
+                    <div className='products_page_item_container'>
+                      <div className='products_page_item_info_container'>
+                        <div className='products_page_item_image_wrapper'>
+                          <div>
+                            <div className='products_page_item_image'>
+                              <a href="">
+                                <img src="" alt="" />
+                              </a>
+                            </div>
+                          </div>
+                        </div>
+                        <div className='products_page_item_text_wrapper'>
+                          <form action="post">
+                            <input type="hidden" />
+                            <div className='products_page_item_details_wrapper'>
+                              <div className='products_page_item_details_container'>
+                                <div className='products_page_item_price_container'>
+                                  <span className='products_page_item_price'>$44.95</span>
+                                </div>
+                                <h3 className='products_page_item_name'>
+                                  <a href="/products/beef-&-cheese">GYOZAS (50 PC)</a>
+                                </h3>
+                                <div>
+                                  <ul className='products_page_item_descriptions'>
+                                    <li className='products_page_item_description'>
+                                      <p>50 Gyozas (Good for 6 Meals!)</p>
+                                    </li>
+                                    <li className='products_page_item_description'>
+                                      <p>Ready in Just 11 Minutes</p>
+                                    </li>
+                                    <li className='products_page_item_description'>
+                                      <p>Steamer Liners Included</p>
+                                    </li>
+                                  </ul>
+                                </div>
+
+                                <div>
+                                  <div>
+                                    <div>
+                                      <div>
+                                        <input type="hidden" />
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+
+                                <div className='products_page_product_rating_container'>
+                                  <div className='products_page_product_ratings'>
+                                    <div className='products_page_product_rating'>
+                                      <svg className='products_page_product_rating_star'><use></use></svg>
+                                      <svg className='products_page_product_rating_star'><use></use></svg>
+                                      <svg className='products_page_product_rating_star'><use></use></svg>
+                                      <svg className='products_page_product_rating_star'><use></use></svg>
+                                      <svg className='products_page_product_rating_star'><use></use></svg>
+                                      <span className='products_page_product_rating_amount'>(2018)</span>
+                                    </div>
+                                  </div>
+                                </div>
 
                               </div>
                               <div className='products_page_add_button_container'>
