434a70a9b4ab9911d5e0fe5f4176359867f23728
Kevin Chen <kevinch28@gmail.com>
2023-03-12 13:12:12 -0400

    update global font and off canvas styling

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import type { AppProps } from 'next/app'
 import '../utils/icons/font-awesome'
 import '../utils/styles/globals.scss'
 import '../utils/styles/navbar.scss'
@@ -6,20 +7,18 @@ import '../utils/styles/modal.scss'
 import '../utils/styles/cart.scss'
 import '../utils/styles/productsPage.scss'
 import '../utils/styles/productShowPage.scss'
-import type { AppProps } from 'next/app'
-import { Oswald } from '@next/font/google'
-import { useState, useEffect } from 'react';
-import { getCurrentUser } from '../utils/sessionApiUtils';
 import Navbar from '../components/navbar/navbar';
 import Footer from '../components/footer/footer';
+// import { Oswald } from '@next/font/google'
 
-const oswald = Oswald({
-  subsets: ['latin'],
-});
+// const oswald = Oswald({
+//   subsets: ['latin'],
+//   weight: ['200', '300', '400', '500', '600', '700']
+// });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={`${oswald.className}`} >
+    <div className='off_canvas_main_content'>
     {/* <div className={`${oswald.className}`} style={{ backgroundColor: '#a9dde3' }}> */}
       <Navbar />
       <Component {...pageProps} />
