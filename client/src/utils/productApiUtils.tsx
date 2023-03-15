402a1a2f68e328d5c7b0d6a1779b11851f7281a2
Kevin Chen <kevinch28@gmail.com>
2023-03-14 21:55:10 -0400

    removed unused api util

--- client/src/utils/productApiUtils.tsx
+++ client/src/utils/productApiUtils.tsx
@@ -6,22 +6,6 @@ export const getProducts = async () => {
   return response.data;
 };
 
-// Get products based user's cart
-export const getProductsByIds = async (cart: object) => {
-  const promises = cart.products.map(async (product: object) => {
-    const { data } = await axios.get(`${baseUrl}/api/products/${product.productId}`);
-    const cart = {
-      productId: data._id,
-      name: data.name,
-      image: data.image,
-      price: data.price,
-      quantity: product.quantity,
-    }
-    return cart;
-  });
-  return await Promise.all(promises);
-};
-
 export const getProduct = async (productId: string) => {
   return await axios.get(`${baseUrl}/api/products/${productId}`);
 };
