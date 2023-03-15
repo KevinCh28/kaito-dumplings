c066bf4ec5b85bbb5a7760a4b2736a82ddf2dc56
Kevin Chen <kevinch28@gmail.com>
2023-03-14 20:25:39 -0400

    update useEffect dependency

--- client/src/pages/products/[productName].tsx
+++ client/src/pages/products/[productName].tsx
@@ -4,24 +4,10 @@ import { useEffect } from 'react';
 const ProductDetail = () => {
   const router = useRouter();
   const productName = (router.query.productName as string);
-  const products: { [key: string]: boolean } = {
-    'dumplings-veggie': true,
-    'dumplings-chicken-&-cabbage': true,
-    'dumplings-beef-&-cheese': true,
-    'dumplings-pork-&-chieves': true,
-    'gyoza-veggie': true,
-    'gyoza-chicken-&-cabbage': true,
-    'gyoza-beef-&-cheese': true,
-    'gyoza-pork-&-chieves': true
-  };
 
   useEffect(() => {
-    if (!products[productName]) {
-      router.push('/products');
-    } else {
-      { productName };
-    };
-  }, [productName, router]);
+    { productName };
+  }, [productName, router.isReady]);
 };
 
 export default ProductDetail;
