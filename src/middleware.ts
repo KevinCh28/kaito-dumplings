6a82135022b64eab7204a785aa25f6dcf73d869d
Kevin Chen <kevinch28@gmail.com>
2023-03-23 20:09:36 -0400

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { verfiyGuest, createGuestToken } from './lib/guestAuth';
+import { useUser } from '@auth0/nextjs-auth0/client';
 
 export async function middleware(request: NextRequest) {
+  const { user } = useUser();
   const token = request.cookies.get('guest')?.value;
   const response = NextResponse.next();
 
@@ -10,6 +12,13 @@ export async function middleware(request: NextRequest) {
     (await verfiyGuest(token).catch((err) => {
       console.log(err);
   }));
+
+  if (user && verifiedToken) {
+    response.cookies.delete('guest');
+    return response;
+  } else if (user) {
+    return;
+  }
 
   if (!verifiedToken) {
     const newToken = await createGuestToken();
