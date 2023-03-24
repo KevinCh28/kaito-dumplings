dca18a58c9aafe223e38182af05736d0c12689b2
Kevin Chen <kevinch28@gmail.com>
2023-03-23 22:40:40 -0400

    update token

--- src/lib/guestAuth.ts
+++ src/lib/guestAuth.ts
@@ -23,7 +23,28 @@ export const verfiyGuest = async (token: string) => {
 };
 
 export const createGuestToken = async () => {
-  const randomString = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  let randomString = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+
+  const readData = await fetch(`${process.env.MONGODB_DATA_API_URL}/action/findOne`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Request-Headers": "*",
+      "api-key": process.env.API_KEY as string,
+    },
+    body: JSON.stringify({
+      dataSource: process.env.MONGODB_DATA_SOURCE as string,
+      database: "test",
+      collection: "guests",
+      filter: { _id: randomString },
+    }),
+  });
+  const readDataJson = await readData.json();
+
+  while (readDataJson._id) {
+    randomString = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  }
+
   const jwtToken = await new SignJWT({ jti: randomString })
     .setProtectedHeader({ alg: 'HS256' })
     .setIssuedAt()
