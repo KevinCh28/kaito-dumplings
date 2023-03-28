f6a8d2479e9a3f1ca44be4fcd9c8d94c2d6046ac
Kevin Chen <kevinch28@gmail.com>
2023-03-28 12:36:27 -0400

    remove getstaticprops

--- src/pages/products/[productName].tsx
+++ src/pages/products/[productName].tsx
@@ -1,16 +1,7 @@
 import { useRouter } from 'next/router';
-import { useEffect, useMemo } from 'react';
+import { useEffect } from 'react';
 
-const flavors: { [key: string]: string } = useMemo(() => {
-  return {
-    'beef-&-cheese': '63efa8319010d97ce1747153',
-    'chicken-&-cabbage': '63efa8b49010d97ce1747155',
-    'pork-&-chieves': '63efa8d19010d97ce1747157',
-    'veggie': '63efa9259010d97ce174715f',
-  };
-}, []);
-
-const ProductDetail = ({ product }) => {
+const ProductDetail = () => {
   const router = useRouter();
   const productName = (router.query.productName as string);
 
@@ -20,10 +11,3 @@ const ProductDetail = ({ product }) => {
 };
 
 export default ProductDetail;
-
-export async function getStaticProps({ params }) {
-  let dumplingId = flavors[params.productName];
-  const res = await fetch(`/api/product?id=${dumplingId}`)
-  const product = await res.json()
-  return { props: { product } }
-}
