8b4b705e5c7680f6b87a44e71198571e4b36ca17
Kevin Chen <kevinch28@gmail.com>
2023-03-30 13:00:49 -0400

    update shipping page styling

--- src/pages/shipping/index.tsx
+++ src/pages/shipping/index.tsx
@@ -2,12 +2,11 @@ import { NextPage } from 'next';
 import { useEffect } from 'react';
 
 const Shipping: NextPage = () => {
+  // Should move this to navbar component and use if statement for each page to change background color and title accordingly
   useEffect(() => {
-    document.title = 'Kaito | Shipping';
+    document.title = 'Kaito | Shipping Info';
     const element = window.document.getElementsByClassName('navbar_main')[0] as HTMLDivElement;
     element.style.backgroundColor = 'rgb(212, 255, 208)';
-    const element2 = window.document.getElementsByClassName('off_canvas_main_content')[0] as HTMLDivElement;
-    element2.style.backgroundColor = 'rgb(212, 255, 208)';
   }, []);
 
   return (
